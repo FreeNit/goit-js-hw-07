33f08a14e556cf2dc7bf65d67da5d13ba7e0464a
Dimon <bolilui.dmytro@gmail.com>
2023-01-03 11:37:37 +0200

    Task1 - Subtask 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// ! 1 - Creation and rendering markup
+
+// * get gallery container
+const galleryEl = document.querySelector('.gallery');
+
+// * Function - create gallery item
+function createGalleryItem(item) {
+  const { preview, original, description } = item;
+  let itemToInsert = `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+
+  return itemToInsert;
+}
+
+// for (const item of galleryItems) {
+//   const { preview, original, description } = item;
+//   let itemToInsert = `
+//     <div class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </div>
+//   `;
+
+//   imagesWrapper.push(itemToInsert);
+// }
+
+// * Container for all elements
+const imagesWrapper = galleryItems.map(createGalleryItem).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesWrapper);
+
+// ! 2 - Delegation and receiving url from big img (prevent default behavior for the link element)
