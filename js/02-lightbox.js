d75f766a4b21f3b18d15348c419bdf28e795e547
Dimon <bolilui.dmytro@gmail.com>
2023-01-04 17:24:01 +0200

    HW7 - Task2 - Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// ! 1 - Creation and rendering markup
+
+// * get gallery container
+const galleryEl = document.querySelector('.gallery');
+
+// * Function - create gallery item
+function createGalleryItem(item) {
+  const { preview, original, description } = item;
+  let itemToInsert = `
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title = "${description}"
+      />
+    </a>
+  `;
+
+  return itemToInsert;
+}
+
+// * Container for all elements
+const imagesWrapper = galleryItems.map(createGalleryItem).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesWrapper);
+
+// ! 2 - Delegation (prevent default behavior for the link element)
+
+// * Prevent default behavior for the links <a>
+const links = document.querySelectorAll('.gallery__link');
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+// * Delegation
+galleryEl.addEventListener('click', onTargetImgClick);
+
+function onTargetImgClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  // * use SimpleLightbox library
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captionDelay: 300,
+  });
+}
